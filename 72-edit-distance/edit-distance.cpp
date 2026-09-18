7c0a9faa22ff6b00a3a6cdd89283f77e9e03c931
himanshu012682-collab <himanshu012682@gmail.com>
2026-09-18 13:29:00 +0530

    Time: 67 ms (7.53%) | Memory: 83.3 MB (6.05%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -23,21 +23,7 @@ int solMem(string s1,string s2,int m,int n,vector<vector<int>>&dp){
     }
     return dp[m][n];
 }
-int rec(string s1,string s2,int m,int n){
-    if(m==0){
-        return n;
-    }
-    if(n==0){
-        return m;
-    }
-    if(s1[m-1]==s2[n-1]){
-        return rec(s1,s2,m-1,n-1);
-    }
-    int insert=rec(s1,s2,m,n-1);
-    int del=rec(s1,s2,m-1,n);
-    int replace=rec(s1,s2,m-1,n-1);
-    return 1+ min(insert,min(del,replace));
-}
+
     int minDistance(string word1, string word2) {
         int m=word1.length();
         int n=word2.length();
